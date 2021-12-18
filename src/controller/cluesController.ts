6bb44bcb120e1f09d334acd0842ac368e9266df3
Ivo Augusto Wanderley de Paiva <40743787+ivomastre@users.noreply.github.com>
2021-12-18 20:22:40 -0300

    chore: added show route

--- src/controller/cluesController.ts
+++ src/controller/cluesController.ts
@@ -1,21 +1,29 @@
-import { IGenericPagination } from "../dto/genericPagination.type"
-import { CluesModel } from "../models"
-import { IClues } from "../models/clues.model"
-
+import { IGenericPagination } from "../dto/genericPagination.type";
+import { CluesModel } from "../models";
+import { IClues } from "../models/clues.model";
 
 export class CluesController {
-    async list({limit, offset}){
-        return {
-            pagination: {
-                limit,
-                offset,
-                total: await CluesModel.countDocuments()
-            },
-            data: await CluesModel.find().limit(Number(limit)).skip(Number(offset)).sort({ createdAt: -1 }),
-        }
-    }
+  async list({ limit, offset }) {
+    return {
+      pagination: {
+        limit,
+        offset,
+        total: await CluesModel.countDocuments(),
+      },
+      data: await CluesModel.find()
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .sort({ createdAt: -1 }),
+    };
+  }
 
-    async create(clue: IClues) {
-        return await CluesModel.create(clue)
-    }
+  async show(id: string) {
+    return {
+      data: await CluesModel.findById(id),
+    };
+  }
+
+  async create(clue: IClues) {
+    return await CluesModel.create(clue);
+  }
 }
--- src/routes/clues.routes.ts
+++ src/routes/clues.routes.ts
@@ -1,44 +1,65 @@
-import 'reflect-metadata'
-import express, { Router, Request } from 'express';
-import { CluesController } from '../controller/cluesController';
-import { IGenericPagination } from '../dto/genericPagination.type';
-import { joiMiddleware } from '../utils/joiMiddleware';
-import Joi from 'joi';
-import multer from '../config/multerConfig'
-import { ICreateClueRequest } from '../@types/routes/clues';
-import uploadToImgur from '../utils/uploadToImgur';
-import { handleAsyncErr } from '../utils/routeErrorHandler';
+import "reflect-metadata";
+import express, { Router, Request } from "express";
+import { CluesController } from "../controller/cluesController";
+import { IGenericPagination } from "../dto/genericPagination.type";
+import { joiMiddleware } from "../utils/joiMiddleware";
+import Joi from "joi";
+import multer from "../config/multerConfig";
+import { ICreateClueRequest } from "../@types/routes/clues";
+import uploadToImgur from "../utils/uploadToImgur";
+import { handleAsyncErr } from "../utils/routeErrorHandler";
 const cluesRoutes = Router();
+const controller = new CluesController();
 
 cluesRoutes.get(
-  '/clues',
-  joiMiddleware(Joi.object({
-    query: {
-      limit: Joi.number().integer().min(1).max(20).default(10),
-      offset: Joi.number().integer().min(0).default(0),
-    }
-  })),
-  handleAsyncErr(async (req: IGenericPagination, res) => { 
-    const controller = new CluesController();
-    return res.json(await controller.list(req.query))
+  "/clues",
+  joiMiddleware(
+    Joi.object({
+      query: {
+        limit: Joi.number().integer().min(1).max(20).default(10),
+        offset: Joi.number().integer().min(0).default(0),
+      },
+    })
+  ),
+  handleAsyncErr(async (req: IGenericPagination, res) => {
+    return res.json(await controller.list(req.query));
+  })
+);
+
+cluesRoutes.get(
+  "/clues/:id",
+  joiMiddleware(
+    Joi.object({
+      params: {
+        id: Joi.string().required(),
+      },
+    })
+  ),
+  handleAsyncErr(async (req, res) => {
+    const id = req.params.id;
+
+    return res.json(await controller.show(id));
   })
 );
 
 cluesRoutes.post(
-  '/clues',
-  multer.array('photos', 20),
-  joiMiddleware(Joi.object({
-    body: {
-      title: Joi.string().required(),
-      description: Joi.string()
-    }
-  })),
+  "/clues",
+  multer.array("photos", 20),
+  joiMiddleware(
+    Joi.object({
+      body: {
+        title: Joi.string().required(),
+        description: Joi.string(),
+      },
+    })
+  ),
   handleAsyncErr(async (req: ICreateClueRequest, res) => {
-    const controller = new CluesController();
-    return res.json(await controller.create({
-      photos: await uploadToImgur(req.files, { ...req.body }),
-      ...req.body
-    }))
+    return res.json(
+      await controller.create({
+        photos: await uploadToImgur(req.files, { ...req.body }),
+        ...req.body,
+      })
+    );
   })
 );
 export default cluesRoutes;
